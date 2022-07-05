c2b87ca151f14b60e9b7d963ba0ec4e3738e8820
torone89 <torone89>
2022-07-05 13:31:34 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+// VERIFICA COLLEGAMENTO JAVA
+console.log("JS")
+
+// esercizio di oggi: Our Team
+// nome repo: js-our-team
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// # MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// # MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+// # MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// # BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva (le trovate in allegato)
+// # BONUS 2:
+// Organizzare i singoli membri in card/schede e ispirarci alle grafiche in allegato
+// Consigli del giorno:
+// Ragioniamo come sempre a step.
+// Prima la logica in italiano e poi traduciamo in codice.
+// E ricordiamoci che console.log() è nostro amico!
+// Buon lavoro! */
+// // # DATI
